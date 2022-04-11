1bf9ee93b598cf9881c807f888cd6229a7267ccf
Jose Francisco <josefranciscosanchez1988@gmail.com>
2022-04-11 18:29:42 +0200

    launch pipepline

--- src/main/java/com/josefsg/crud/dto/Mensaje.java
+++ src/main/java/com/josefsg/crud/dto/Mensaje.java
@@ -14,4 +14,6 @@ public class Mensaje {
     public void setMensaje(String mensaje) {
         this.mensaje = mensaje;
     }
+
+    public void executeMensaje(String mensaje) {this.mensaje = mensaje;}
 }
